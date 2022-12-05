b85acf79f065721831e7cc6f108a3ddd131fc8cd
Carlos Daza <cdaza@ontop.ai>
2022-12-05 00:44:53 -0500

    api gateway stated

---
+++ src/test/java/com/parking/gateway/GateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parking.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
